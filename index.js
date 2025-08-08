dadcabcec02d74d3824f3cc6e57efd082de36df5
Louis-CharlesBiron <louirlesbiron6@gmail.com>
2025-08-08 15:17:54 -0400

    Game setup progress

---
+++ index.js
@@ -0,0 +1,22 @@
+const fpsCounter = new FPSCounter(), CVS = Canvas.create(null, ()=>{//looping
+    let fps = fpsCounter.getFps()+"\n"+fpsCounter.fpsRaw
+    if (fpsDisplay.textContent !== fps) fpsDisplay.textContent = fps
+})
+
+// DECLARE OBJS
+
+
+
+
+
+// USER ACTIONS
+const mMove=m=>mouseInfo.textContent = "("+m.x+", "+m.y+")"
+CVS.setMouseMove(mMove)
+CVS.setMouseLeave(mMove)
+CVS.setMouseDown()
+CVS.setMouseUp()
+CVS.setKeyDown()
+CVS.setKeyUp()
+
+// START
+CVS.start()
